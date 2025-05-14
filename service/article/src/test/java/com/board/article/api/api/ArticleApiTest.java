37b0abcd091596e19da80b29adfd746433d29315
baejiwon <qowhxk@naver.com>
2025-05-14 21:06:22 +0900

    무한 스크롤 기능 Test Code 구현

--- service/article/src/test/java/com/board/article/api/api/ArticleApiTest.java
+++ service/article/src/test/java/com/board/article/api/api/ArticleApiTest.java
@@ -4,9 +4,14 @@ import com.board.article.domain.dto.request.ArticleCreateRequest;
 import com.board.article.domain.dto.request.ArticleUpdateRequest;
 import com.board.article.domain.dto.response.ArticlePageResponse;
 import com.board.article.domain.dto.response.ArticleResponse;
+import com.board.article.domain.entity.Article;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
+
+import java.util.List;
 
 public class ArticleApiTest {
     RestClient restClient = RestClient.create("http://localhost:9000");     // api 요청 가능 테스트
@@ -71,6 +76,7 @@ public class ArticleApiTest {
     }
 
     @Test
+    @DisplayName("게시물 페이징 - Covering Index")
     void readAllTest(){
         ArticlePageResponse response = restClient.get()
                 .uri("/api/v1/articles?boardId=1&pageSize=30&page=50000")
@@ -80,6 +86,33 @@ public class ArticleApiTest {
         System.out.println("Read All Post ResponseCount() =  "+response.articleCount());
         for(ArticleResponse article : response.articles()){
             System.out.println("articleId = "+article.articleId());
+        }
+    }
+
+    @Test
+    @DisplayName("무한 스크롤 페이징")
+    void readAllInfiniteScrollTest(){
+        List<ArticleResponse> articles = restClient.get()
+                .uri("/api/v1/articles/infinite-scroll?boardId=1&pageSize=5")
+                .retrieve()
+                // ParameterizedTypeReference : 제네릭 타입으로 객체를 반환해주기 위해 사용
+                .body(new ParameterizedTypeReference<List<ArticleResponse>>(){
+                });
+
+        System.out.println("first Page");
+        for(ArticleResponse article : articles){
+            System.out.println("articleResponse.getArticleId() =  "+article.articleId());
+        }
+
+        Long lastArticleId = articles.getLast().articleId();
+        List<ArticleResponse> article2  = restClient.get()
+                .uri("/api/v1/articles/infinite-scroll?boardId=1&pageSize=5&lastArticleId=%s".formatted(lastArticleId))
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<ArticleResponse>>(){});
+
+        System.out.println("second Page");
+        for(ArticleResponse article : article2){
+            System.out.println("articleResponse.getArticleId() =  "+article.articleId());
         }
     }
 }
--- service/article/src/test/java/com/board/article/repository/ArticleRepositoryTest.java
+++ service/article/src/test/java/com/board/article/repository/ArticleRepositoryTest.java
@@ -2,6 +2,7 @@ package com.board.article.repository;
 
 import com.board.article.domain.entity.Article;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,4 +34,18 @@ class ArticleRepositoryTest {
         log.info("count = {}", count);
     }
 
+    @Test
+    @DisplayName("무한 스크롤")
+    void findInfiniteScrollTest(){
+        List<Article> articles = articleRepository.findAllInfiniteScroll(1L, 30L);
+        for(Article article : articles){
+            log.info("articleId = {}", article.getArticleId());
+        }
+
+        Long lastArticleId = articles.getLast().getArticleId();
+        List<Article> articles2 = articleRepository.findAllInfiniteScroll(1L,30L,lastArticleId);
+        for(Article article : articles2){
+            log.info("articleId = {}", article.getArticleId());
+        }
+    }
 }
