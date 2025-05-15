ae162a9489d75236bacf945967411af32740ad8e
baejiwon <qowhxk@naver.com>
2025-05-15 23:42:14 +0900

    Comment Entity 구현

---
+++ service/comment/src/main/java/com/board/comment/domain/entity/Comment.java
@@ -0,0 +1,48 @@
+package com.board.comment.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+    @Id
+    private Long commentId;
+    private String content;
+    private Long parentCommentId;
+    private Long articleId;
+    private Long writerId;
+    private boolean deleted;
+    private LocalDateTime createdAt;
+
+    public static Comment create(Long commentId, String content, Long parentCommentId, Long articleId, Long writerId) {
+        Comment comment = new Comment();
+        comment.commentId = commentId;
+        comment.content = content;
+        comment.parentCommentId = parentCommentId == null ? commentId : parentCommentId;
+        comment.articleId = articleId;
+        comment.writerId = writerId;
+        comment.deleted = false;
+        comment.createdAt = LocalDateTime.now();
+        return comment;
+    }
+
+    // 현재 댓글이 최상위 댓글인지 확인
+    public boolean isRoot(){
+        return parentCommentId.longValue() == commentId;
+    }
+
+    public void delete(){
+        deleted = true;
+    }
+}
