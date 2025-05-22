a075d6636634b074987bb85d4fed6222de9ebd2c
baejiwon <qowhxk@naver.com>
2025-05-22 13:58:53 +0900

    페이징 번호 구하는 공식 추가

---
+++ service/comment/src/main/java/com/board/comment/service/PageLimitCalculator.java
@@ -0,0 +1,18 @@
+package com.board.comment.service;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+// 페이지마다 개수 구분 해주는 service
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PageLimitCalculator {
+
+    /*
+       page : 특정 페이지 요청
+       pageSize : 한 페이지당 N개 게시글
+       movablePageCount : 페이지네이션 UI에 N개 페이지 번호 표시 (10이면 총 10Page 출력)
+     */
+    public static Long calculatePageLimit(Long page, Long pageSize, Long movablePageCount){
+        return (((page - 1) / movablePageCount) + 1) * pageSize * movablePageCount + 1;         // 페이징 처리 공식
+    }
+}
