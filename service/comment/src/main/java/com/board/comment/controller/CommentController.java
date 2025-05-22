c8123b450074a24cb0d538c4fa3f355c66778e94
baejiwon <qowhxk@naver.com>
2025-05-22 13:58:19 +0900

    댓글 페이징, 무한 스크롤 api 추가

--- service/comment/src/main/java/com/board/comment/controller/CommentController.java
+++ service/comment/src/main/java/com/board/comment/controller/CommentController.java
@@ -1,11 +1,14 @@
 package com.board.comment.controller;
 
 import com.board.comment.domain.dto.request.CommentCreateRequest;
+import com.board.comment.domain.dto.response.CommentPageResponse;
 import com.board.comment.domain.dto.response.CommentResponse;
 import com.board.comment.domain.entity.Comment;
 import com.board.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +31,25 @@ public class CommentController {
     @DeleteMapping("/{commentId}")
     public void delete(@PathVariable Long commentId){
         commentService.delete(commentId);
+    }
+
+
+    @GetMapping()
+    public CommentPageResponse readAll(
+            @RequestParam("articleId") Long articleId,
+            @RequestParam("page")Long page,
+            @RequestParam("pageSize")Long pageSize
+    ){
+        return commentService.readAll(articleId, page, pageSize);
+    }
+
+    @GetMapping("/infinite-scroll")
+    public List<CommentResponse> readAll(
+            @RequestParam("articleId") Long articleId,
+            @RequestParam(value = "lastParentCommentId", required = false)Long lastParentCommentId,
+            @RequestParam(value = "lastCommentId", required = false)Long lastCommentId,
+            @RequestParam("pageSize")Long pageSize
+    ){
+        return commentService.readAll(articleId, lastParentCommentId, lastCommentId, pageSize);
     }
 }
